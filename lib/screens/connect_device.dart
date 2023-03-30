1b7fc6aed2fe39c453cb56f9bf1384283b80ec63
Imanol Gutierrez <developer@imanol.org>
2023-03-30 23:38:21 +0200

    Fix bluetooth problems for Android

--- lib/screens/connect_device.dart
+++ lib/screens/connect_device.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 
 class ConnectDevice extends StatefulWidget {
   const ConnectDevice({Key? key}) : super(key: key);
@@ -10,9 +9,24 @@ class ConnectDevice extends StatefulWidget {
 }
 
 class _ConnectDeviceState extends State<ConnectDevice> {
-  final flutterReactiveBle = FlutterReactiveBle();
+  FlutterBlue flutterBlue = FlutterBlue.instance;
   @override
   Widget build(BuildContext context) {
+    // Start scanning
+    flutterBlue.startScan(timeout: const Duration(seconds: 4));
+
+// Listen to scan results
+    var subscription = flutterBlue.scanResults.listen((results) {
+      // do something with scan results
+      print("Entrando bucle -------");
+      for (ScanResult r in results) {
+        print('${r.device.name} found! rssi: ${r.rssi}');
+      }
+      print("Saliendo bucle -------");
+    });
+
+// Stop scanning
+    flutterBlue.stopScan();
     return Scaffold();
   }
 }
